4937eb6dccdc082023401545c4928eb7440d1dbb
Kasmas <kasmas.kosmos@gmail.com>
2018-06-27 22:21:01 +0700

    main func, pr

---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "deposit2.h"
+#include "deposit.h"
+
+int main() {
+    int deposit, time;
+
+    printf("Enter deposit and time\n");
+    do {
+    	scanf("%d %d", &deposit, &time);
+    } while (Check (deposit, time), printf("invalid data"));
+
+    printf("v: %d s: %d\n", deposit, time);
+    
+    printf("\nprofit = %d\n", Profit(deposit, time));
+
+    return 0;
+}
